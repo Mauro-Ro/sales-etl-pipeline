52e317fddb7c13b0af66eb9b692297e286683abd
Mauro <mausrosales@gmail.com>
2025-12-30 09:11:36 -0300

    WIP:New logic for extract_data.py

--- extract/extract_data.py
+++ extract/extract_data.py
@@ -1,4 +1,5 @@
 from transform.validation import validator_extract
+import requests
 import pandas as pd
 
 
@@ -6,6 +7,21 @@ path_csv = "extract/data/Kaggle_Books-Selling-Records-.csv"
 
 def extract(pd, path: str):
     return validator_extract(pd, path)
+
+
+
+
+def request_path(path: str):
+    return requests.get(path).json()
+
+carts = request_path("https://dummyjson.com/carts")
+
+users = request_path("https://dummyjson.com/users")
+
+products = request_path("https://dummyjson.com/products")
+
+
+
 
 
 
